c098269470129c2c2be6ede7a976dffe77c8a43b
aynoh <anyoung.noh@gmail.com>
2022-07-06 09:30:50 +0900

    CHG: 코드정리

--- src/aoc2018_day1.clj
+++ src/aoc2018_day1.clj
@@ -1,14 +1,20 @@
 (ns aoc2018-day1
   (:require [clojure.string]))
 
+(def input-data (->> (slurp "data/aoc2018_day1_data")
+                     (clojure.string/split-lines)
+                     (map #(Integer/parseInt %))))
 
+;; --------------
+;; part 1
 
-(defn part-1
+(defn solve-part1
   [items]
   (reduce + items))
 
 
-;; 주어진 컬렉션에서 반복되는 원소가 있으면 이를 반환하는 함수
+;; --------------
+;; part 2
 
 (defn get-first-duplicated
   [coll]
@@ -19,24 +25,21 @@
           #{}
           coll))
 
-;; 주어진 컬렉션을 누적 합산한 lazy-seq를 만들고 이를 get-first-duplicated 의 인자로 넘긴다
-(defn part-2
+(defn solve-part2
   [coll]
   (let [freqs (->> coll cycle (reductions + 0))]
     (get-first-duplicated freqs)))
 
 
 (comment
-  (def input-data (->> (slurp "data/aoc2018_day1_data")
-                       (clojure.string/split-lines)
-                       (map #(Integer/parseInt %))))
-  (part-1 input-data)
-  (part-2 input-data)
-)
+  (solve-part1 input-data)
+  (solve-part2 input-data))
 
 
 
-(defn part-2-with-loop
+;; -------------------------------
+;; loop, recur / reduce를 이용한 풀이
+(defn solve-part2-with-loop1
   [items]
   (loop [result-set #{}
          acc 0
@@ -46,17 +49,16 @@
       (recur (conj result-set (+ acc x))
              (+ acc x)
              coll'))))
-)
 
-(defn part-2-refactored
+(defn solve-part2-with-loop2
   [items]
   (loop [freq-history (->> items cycle (reductions + 0))
-        seen-freqs #{}]
+         seen-freqs #{}]
     (if (seen-freqs (first freq-history))
       (first freq-history)
       (recur (rest freq-history) (conj seen-freqs (first freq-history))))))
 
-(defn part-2-refactored2
+(defn solve-part2-with-reduce
   [items]
   (let [freq-history (->> items cycle (reductions + 0))]
     (reduce (fn [seen-freqs new-val]
@@ -66,66 +68,8 @@
             #{}
             freq-history)))
 
-(defn find-first-duplicated
-  [coll]
-  (reduce (fn [seen-set new-val]
-              (if (seen-set new-val)
-                (reduced new-val)
-                (conj seen-set new-val)))
-            #{}
-            coll))
-
-(defn part-2-refactored3
-  [items]
-  (let [freq-history (->> items cycle (reductions + 0))]
-    (find-first-duplicated freq-history)))
-
-
-
-(reduce (fn [a b] (reduced :this-is-my-answer))
-        (iterate inc 0))
-
-(reduce + [1 2 3 4 5])
-
-
-    
-
 
 (comment
-(def input-data (->> (slurp "aoc2018_day1_data")
-                     (clojure.string/split-lines)
-                     (map #(Integer/parseInt %))))
-(part-2 [+3, +3, +4, -2, -4, +4])
-(part-2-refactored [+3, +3, +4, -2, -4, +4])
-(part-2-refactored input-data)
-(part-2-refactored2 input-data)
-(part-2-refactored3 input-data)
-
-(conj '(1 2 3 4) 5)
-(reductions + [+3, +3, +4, -2, -4, +4])
-(part-2 input-data)
-
-
-)
-
-
-
-
-(comment
-  (reductions + [0, +3, +3, +4, -2, -4])
-  (reductions + [0, -6, +3, +8, +5, -6])
-  (reductions + [0, +1, -1])
-  (contains? 1 '(1 2 3))
-  (conj #{1 2 3} 4)
-
-(def data (cycle input-data))
-(part-2 data)
-
-  
-  
-)
-
-
-                       
- 
-  
+  (solve-part2-with-loop1 input-data)
+  (solve-part2-with-loop2 input-data)
+  (solve-part2-with-reduce input-data))
